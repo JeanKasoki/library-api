e7d9d3701e9967ad4d722140b5af7f8b28ae53b5
JeanKasoki <jeanmyjean@hotmail.com>
2026-01-17 23:54:19 -0300

    feat(database): connect to mysql and implement auto migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,4 +1,22 @@
 package main
 
+import(
+	"fmt"
+	"github.com/JeanKasoki/library-api/internal/entity"
+	"github.com/JeanKasoki/library-api/internal/infra/database"
+)
+
 func main(){
+	// Capturamos a conexão (db) e o erro (err) para tentar conectar no banco
+	db, err := database.ConnectDB()
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println("Conexão com o banco de dados realizada com sucesso.")
+	// Criação das auto migrations, passamos o db de cima capturado e passamos um PONTEIRO (&) de cada entidade vazia
+	err = db.AutoMigrate(&entity.Book{}, &entity.Loan{}, &entity.Notification{}, &entity.User{})
+	if err != nil{
+		panic("Falha em realizar a migração: " + err.Error())
+	}
+	fmt.Println("Tabelas criadas/atualizadas com sucesso.")
 }
--- internal/infra/database/connection.go
+++ internal/infra/database/connection.go
@@ -10,7 +10,7 @@ func ConnectDB() (*gorm.DB, error) {
   // DSN (Data Source Name): A "URL" de conexão do MySQL
 	// parseTime=True é OBRIGATÓRIO para funcionar com as structs que usam time.Time
 	// user:password@protocol(address:port)/database?settings
-  dsn := "root:root@tcp(127.0.0.1:3306)/biblioteca?charset=utf8mb4&parseTime=True&loc=Local"
+  dsn := "root:root@tcp(127.0.0.1:3306)/library?charset=utf8mb4&parseTime=True&loc=Local"
 	// Abre a conexão usando o driver do MySQL
   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
