756dc3690d00b0c64d057d271afe40b1963abf31
JeanKasoki <jeanmyjean@hotmail.com>
2026-01-29 01:08:30 -0300

    feat(handler): add list books handler and update struct dependencies

--- internal/infra/web/handler/book_handler.go
+++ internal/infra/web/handler/book_handler.go
@@ -9,14 +9,18 @@ import (
 
 // Ele sabe traduzir HTTP (JSON) para Go e vice-versa.
 type BookHandler struct {
-	API *usecase.CreateBookUseCase
+	CreateBookUseCase *usecase.CreateBookUseCase
+	ListBooksUseCase *usecase.ListBooksUseCase
 }
 
-func NewBookHandler(api *usecase.CreateBookUseCase) *BookHandler{
-	return &BookHandler{API: api}
+func NewBookHandler(create *usecase.CreateBookUseCase, list *usecase.ListBooksUseCase) *BookHandler{
+	return &BookHandler{
+		CreateBookUseCase: create,
+		ListBooksUseCase: list,
+	}
 }
 
-func (bookHandler *BookHandler) Create(w http.ResponseWriter, req *http.Request) {
+func (h *BookHandler) Create(w http.ResponseWriter, req *http.Request) {
 	log.Debug().Msg("A requisição chegou no controller")
 	
 	// passo 1: criar DTO do book
@@ -35,7 +39,7 @@ func (bookHandler *BookHandler) Create(w http.ResponseWriter, req *http.Request)
 	}
 
 	// passo 3: Passamos os dados limpos pro UseCase validar e mandar salvar
-	err = bookHandler.API.ExecuteAsync(&inputBook)
+	err = h.CreateBookUseCase.Execute(&inputBook)
 	if err != nil{
 		// LOG: Error. Ocorreu erro de regra de negócio
 		log.Error().Msg("Application have been failed")
@@ -48,4 +52,25 @@ func (bookHandler *BookHandler) Create(w http.ResponseWriter, req *http.Request)
 
 	// passo 4: responder o cliente, Resposta HTTP 201 (Created)
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *BookHandler) List (w http.ResponseWriter, req *http.Request){
+	// 1. Chamar o UseCase (h.ListBooksUseCase.Execute())
+	list, err := h.ListBooksUseCase.Execute()
+    if err != nil {
+			log.Error().Msg("Falha ao encontrar livros")
+			// Respondendo 500 para o usuário
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		// // Log de sucesso (apenas informativo). Usamos len(list) para dizer quantos livros achamos
+		log.Info().Msgf("Sucesso! Livros encontrados: %d", len(list))
+    // 3. Sucesso (Responder JSON)
+		// A: Avisar o navegador/Postman que o que vem aí é JSON
+		w.Header().Set("Content-Type", "application/json")
+		// B: Definir status 200 (OK)
+		w.WriteHeader(http.StatusOK)
+    // C: Converter a lista (Go) para JSON (Texto) e escrever na resposta (w)
+		// NewEncoder conecta com a saída (w). Encode pega o dado (list).
+		json.NewEncoder(w).Encode(list)
 }
